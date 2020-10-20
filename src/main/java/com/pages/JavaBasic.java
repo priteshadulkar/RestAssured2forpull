1e99f50c7a953dfd3c4639b2dbfeb1db158b5226
Pritesh Adulkar <adulkarritesh@gmail.com>
2020-10-20 15:10:28 +0530

    ready for the pull request

--- src/main/java/com/pages/JavaBasic.java
+++ src/main/java/com/pages/JavaBasic.java
@@ -1,5 +1,49 @@
 package com.pages;
 
-public class JavaBasic {
+import org.testng.annotations.Test;
 
+public class JavaBasic {
+	@Test
+	public void NativeAPpAndroid()
+	{
+		System.out.println("NativeAPpAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");	
+	}
+	
+	@Test
+	public  void pal()
+	{
+		int counter = 0 ;
+		for(int i =1; i<=100 ; i++)
+		{
+			counter = 0 ;
+			for(int j=i ; j>=1 ; j--)
+			{
+				if(i%j==0)
+				{
+					counter =  counter + 1 ;
+					
+				}
+			}
+				
+		    if(counter == 2)
+		    {
+		    	//System.out.println("the prime number is");
+		    	System.out.print(i +" ");
+		    	
+		    	
+		    }
+				
+			
+		}
+		
+		
+		
+		
+	}
 }
